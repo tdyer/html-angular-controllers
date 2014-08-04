1bff31e45ec6b876c0182e5ff09342a8ce2723ec
Tom Dyer <tdyer1@gmail.com>
2014-08-04 18:35:08 -0400

    Added a route for customers and a default route.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 (function customersAppIIFE(){
-  // Create a Module for this app with a name of 'customersApp'
-  // It has NO dependencies, empty Array as the second param
   var app = angular.module('customersApp', ['ngRoute']);
 
-  app.module.config(function($routeProvider){
+  app.config(function($routeProvider){
     $routeProvider
       .when('/',
             {
